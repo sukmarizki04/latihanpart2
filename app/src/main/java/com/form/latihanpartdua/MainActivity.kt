d49bf51e132944d0b49e846c6c34dfca9bf1d8f0
Sukma Rizki <sukmarizki04@gmail.com>
2022-10-06 09:55:31 +0700

    first

---
+++ app/src/main/java/com/form/latihanpartdua/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.form.latihanpartdua
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var button = findViewById<Button>(R.id.btn_next)
+        button.setOnClickListener {
+            val intent = Intent(this,MainActivity2::class.java)
+            startActivity(intent)
+        }
+    }
+}
